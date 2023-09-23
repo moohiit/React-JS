0ebf41e3efab2322f57f7d5b9f5c992ef4a1a770
Mohit Patel <pmohit645@gmail.com>
2023-09-23 15:12:36 +0530

    Vite+React (Background Changer App)

--- 04bgChanger/src/App.jsx
+++ 04bgChanger/src/App.jsx
@@ -1,33 +1,56 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [color, setColor] = useState("#212121")
 
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
+      <div className="w-full h-screen duration-200"
+      style={{backgroundColor: color}}>
+        <div className='fixed flex flex-wrap justify-center top-12 inset-x-0 gap-3 shadow-lg bg-gray-400 px-3 py-2 rounded-2xl'><p className='outline-none px-4 py-1 rounded-full text-black bg-white font-semibold text-3xl shadow-lg'>Color Changer App</p></div>
+        <div className='fixed flex flex-wrap justify-center bottom-12 inset-x-0 px-2'>
+          <div className='flex flex-wrap justify-center gap-3 shadow-lg bg-white px-3 py-2 rounded-2xl'>
+            <button onClick={() => setColor("red")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "red"}}>
+              Red
+            </button>
+            <button onClick={() => setColor("grey")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "grey"}}>
+              Grey
+            </button>
+            <button onClick={() => setColor("white")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "white"}}>
+              white
+            </button>
+            <button onClick={() => setColor("pink")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "pink"}}>
+              Pink
+            </button>
+            <button onClick={() => setColor("blue")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "blue"}}>
+              Blue
+            </button>
+            <button onClick={() => setColor("yellow")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "yellow"}}>
+              Yellow
+            </button>
+            <button onClick={() => setColor("green")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "green"}}>
+              Green
+            </button>
+            <button onClick={() => setColor("purple")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "purple"}}>
+              Purple
+            </button>
+            <button onClick={() => setColor("orange")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "orange"}}>
+              Orange
+            </button>
+            <button onClick={() => setColor("yellowgreen")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "yellowgreen"}}>
+              Yellowgreen
+            </button>
+            <button onClick={() => setColor("lavender")} className='outline-none px-4 py-1 rounded-full text-black shadow-lg' style={{backgroundColor: "lavender"}}>
+              Lavender
+            </button>
+            <button onClick={() => setColor("black")} className='outline-none px-4 py-1 rounded-full text-white shadow-lg' style={{backgroundColor: "black"}}>
+              Black
+            </button>
+          </div>
+
+        </div>
       </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
     </>
   )
 }
