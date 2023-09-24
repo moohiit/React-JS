10ade4404ecd20f4088131cdbb826e52b5808527
Mohit Patel <pmohit645@gmail.com>
2023-09-24 12:23:10 +0530

    Vite+React (Currency Convertor App)

---
+++ 06currencyConvertor/src/components/index.js
@@ -0,0 +1,11 @@
+
+//This file is created for reducing the import
+//burden different components one by one that's
+//why we use to import all component to this file
+//then we can import all the component in a single 
+//import from App.jsx.
+
+
+import InputBox from "./InputBox";
+
+export default {InputBox};
